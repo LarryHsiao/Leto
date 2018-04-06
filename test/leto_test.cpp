a885200fcc504fe650bee1b72a0e7d56e68e4826
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-06 23:17:48 +0800

    build script added

--- test/leto_test.cpp
+++ test/leto_test.cpp
@@ -2,3 +2,11 @@
 // Created by Larry Hsiao on 4/6/2018.
 //
 
+#include <gtest/gtest.h>
+#include <leto.h>
+
+TEST(Assertion, 123fdsa){
+    auto leto = new Leto();
+    leto->compute();
+    ASSERT_TRUE(true);
+}
