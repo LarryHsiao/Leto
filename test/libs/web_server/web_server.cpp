f32f32bddf8006c8f672d09edc09781011056887
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-12 09:37:44 +0800

    simple usage of webserver in test cpp

--- test/libs/web_server/web_server.cpp
+++ test/libs/web_server/web_server.cpp
@@ -3,7 +3,22 @@
 //
 
 #include <gtest/gtest.h>
+#include <server_http.hpp>
+
+using namespace std;
+using namespace SimpleWeb;
+using HttpServer=Server<HTTP>;
 
 TEST(web_server, server_ok) {
+    HttpServer server;
+    server.config.port=9010;
+    server.resource["^/string$"]["POST"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        auto content = std::string("This is response");
+
+        *response << "HTTP/1.1 200 OK\r\n"
+                  << "Content-Length: " << content.length() << "\r\n\r\n"
+                  << content;
+    };
+//    server.start();
 }
 
