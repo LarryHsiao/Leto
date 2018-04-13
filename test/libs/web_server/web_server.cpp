2fa1e02ce247a3f47e1cd8266a7a9ac318da5f53
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-13 17:44:54 +0800

    simple web server edge test added

--- test/libs/web_server/web_server.cpp
+++ test/libs/web_server/web_server.cpp
@@ -4,21 +4,34 @@
 
 #include <gtest/gtest.h>
 #include <server_http.hpp>
+#include <client_http.hpp>
 
 using namespace std;
 using namespace SimpleWeb;
 using HttpServer=Server<HTTP>;
+using HttpClient=Client<HTTP>;
 
 TEST(web_server, server_ok) {
     HttpServer server;
     server.config.port=9010;
-    server.resource["^/string$"]["POST"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+    server.resource["^/string$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
         auto content = std::string("This is response");
-
         *response << "HTTP/1.1 200 OK\r\n"
                   << "Content-Length: " << content.length() << "\r\n\r\n"
                   << content;
     };
-//    server.start();
+
+    thread thread([&server]() {
+        server.start();
+    });
+
+    this_thread::sleep_for(chrono::seconds(1));
+
+    HttpClient client("localhost:9010");
+    auto response = client.request("GET","/string");
+    auto content = response->content.string();
+    server.stop();
+    thread.join();
+    ASSERT_EQ(string("This is response"), content);
 }
 
