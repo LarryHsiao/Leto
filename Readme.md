df53517c2abd01de3268ece6a1ece2fc39055a4b
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-19 16:09:36 +0800

    remote use_openssl which still failed

--- Readme.md
+++ Readme.md
@@ -3,9 +3,4 @@ Leto
 
 An application which combines all stuff I have.
 
-Build parameter
-===
-When build on Windows with pre-built openssl library.
-Add these parameters:
--DCMAKE_USE_OPENSSL=ON
  
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -DCMAKE_USE_OPENSSL=ON -G "CodeBlocks - MinGW Makefiles" ..
+cmake -G "CodeBlocks - MinGW Makefiles" ..
 cmake --build . --target Leto  -- -j 4
 cmake --build . --target Leto_test  -- -j 4
 
