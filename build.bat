4a6840b4f841383cc2fdb1710abe844d1d973a4a
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-19 18:24:39 +0800

    curl using pre-built version

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -G "CodeBlocks - MinGW Makefiles" ..
+cmake -DCMAKE_INSTALL_PREFIX=%Phantom_Libs% -G "CodeBlocks - MinGW Makefiles" ..
 cmake --build . --target Leto  -- -j 4
 cmake --build . --target Leto_test  -- -j 4
 
