a67078e6b308ce0a45b1aca9432a7cd44e996309
Larry Hsiao <larryhsiao@silverhetch.com>
2018-04-25 17:40:00 +0800

    build script update

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=%Phantom_Libs% -G "CodeBlocks - MinGW Makefiles" ..
-cmake --build . --target Leto  -- -j 4
+cmake --build . --target Leto_win  -- -j 4
 cmake --build . --target Leto_test  -- -j 4
 
 Leto_test.exe
